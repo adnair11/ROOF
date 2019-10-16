6139f705b65db195bcf032a2d3cb02352e8734e7
Aditya Nair <adnair11@in.ibm.com>
2019-10-16 12:29:40 +0530

    Detailed attributes of property

--- source/roof-service/src/main/java/com/ibm/roof/model/Property.java
+++ source/roof-service/src/main/java/com/ibm/roof/model/Property.java
@@ -1,5 +1,7 @@
 package com.ibm.roof.model;
 
+import java.util.Date;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -10,13 +12,148 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 @XmlRootElement
 public class Property {
+	
+	//Auto Generated Mongodb property id
 	@Id
 	String _id;
+	
+	
+	//userid of current user
+	String usrId;
+	
+	//Property details
 	String name;
 	int bhk;
+	String type;
+	int age;
+	int floor;
+	int totalFloors;
+	int size;
+	
+	//locality details
 	String city;
+	String locality;
+	
+	//rental details
+	float rent;
+	float deposit;
+	Date available;
+	String prefTenant;
+	String furnish;
+	String parking;
+	
+	//Amenities 
+	int bathrooms;
+	int balconies;
+	boolean lift;
+	boolean ac;
+	boolean internet;
 	
 	
+	
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getFloor() {
+		return floor;
+	}
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+	public int getTotalFloors() {
+		return totalFloors;
+	}
+	public void setTotalFloors(int totalFloors) {
+		this.totalFloors = totalFloors;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getLocality() {
+		return locality;
+	}
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+	public float getRent() {
+		return rent;
+	}
+	public void setRent(float rent) {
+		this.rent = rent;
+	}
+	public float getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(float deposit) {
+		this.deposit = deposit;
+	}
+	public Date getAvailable() {
+		return available;
+	}
+	public void setAvailable(Date available) {
+		this.available = available;
+	}
+	public String getPrefTenant() {
+		return prefTenant;
+	}
+	public void setPrefTenant(String prefTenant) {
+		this.prefTenant = prefTenant;
+	}
+	public String getFurnish() {
+		return furnish;
+	}
+	public void setFurnish(String furnish) {
+		this.furnish = furnish;
+	}
+	public String getParking() {
+		return parking;
+	}
+	public void setParking(String parking) {
+		this.parking = parking;
+	}
+	public int getBathrooms() {
+		return bathrooms;
+	}
+	public void setBathrooms(int bathrooms) {
+		this.bathrooms = bathrooms;
+	}
+	public int getBalconies() {
+		return balconies;
+	}
+	public void setBalconies(int balconies) {
+		this.balconies = balconies;
+	}
+	public boolean isLift() {
+		return lift;
+	}
+	public void setLift(boolean lift) {
+		this.lift = lift;
+	}
+	public boolean isAc() {
+		return ac;
+	}
+	public void setAc(boolean ac) {
+		this.ac = ac;
+	}
+	public boolean isInternet() {
+		return internet;
+	}
+	public void setInternet(boolean internet) {
+		this.internet = internet;
+	}
 	public String getName() {
 		return name;
 	}
